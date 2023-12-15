a7f1a656b033caf94da739024408c303f7a53907
manu1532 <94106649+manu1532@users.noreply.github.com>
2023-12-15 23:19:59 +0530

    Create 281A.Word Capitalization.cpp

---
+++ 281A.Word Capitalization.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve(){
+    string s;
+    cin >> s;
+    
+    s[0] = toupper(s[0]);
+    
+    cout << s << endl;
+    
+    return ;
+}
+
+int main(){
+    int t = 1;
+    // cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
